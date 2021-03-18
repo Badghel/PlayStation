f099c5a25a0c025a771c3ddcebff5360eda444c0
Badghel <75239876+Badghel@users.noreply.github.com>
2021-03-17 21:43:51 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector('.menu-icon');
+const toggle2 = document.querySelector('.menu-icon');
+toggle.addEventListener('click', function () {
+document.getElementById('nav').classList.toggle('active')
+})
+
